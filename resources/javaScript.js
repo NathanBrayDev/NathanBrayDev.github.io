00ea700da20a673ac0645b19af50a04442719f7b
NathanBrayDev <123970103+NathanBrayDev@users.noreply.github.com>
2023-03-07 11:40:07 +0000

    Add files via upload

--- resources/javaScript.js
+++ resources/javaScript.js
@@ -17,23 +17,35 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" current", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " current";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " current";
 }
 
-function ToggleVisibility(divClass)
-{
-    var els = document.getElementsByClassName(divClass);
-    for(var i = 0; i < els.length; i++)
-    {
-        els[i].style.display = els[i].style.display == "block" ? "none" : "block";
-    }
+function ToggleVisibility(textDivClass, buttonDivClass) {
+  var els = document.getElementsByClassName(textDivClass);
+  for (var i = 0; i < els.length; i++) {
+    els[i].style.display = els[i].style.display == "none" ? "block" : "none";
+  }
+  var button = document.getElementById(buttonDivClass);
+  if (button.style.color != "white") {
+    button.style.color = "white";
+    button.style.backgroundColor = "#8B0000";
+    button.style.padding = "3vh 0";
+  } else {
+    button.style.color = "black";
+    button.style.backgroundColor = "white";
+    button.style.padding = "1vh 0";
+  }
 }
